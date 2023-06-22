2c99d5a4fed9c0e0719fcdbcf60eef4849cb2651
Lucas Baffe Segura <lucas.baffe.segura@accenture.com>
2023-06-22 18:19:16 -0300

    Parte 2

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,74 @@
+﻿using MeuTodo.Data;
+using MeuTodo.Models;
+using MeuTodo.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class TodoController : ControllerBase
+    {
+        public TodoController() { }
+
+        [HttpGet]
+        [Route("todos")]
+        public async Task<IActionResult> GetAsync([FromServices]AppDbContext context)
+        {
+            var todos = await context.Todos
+                                        .AsNoTracking()
+                                        .ToListAsync();
+            
+            return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route("todos/{id}")]
+        public async Task<IActionResult> GetByIdAsync([FromServices] AppDbContext context,
+                                                      [FromRoute]int id)
+        {
+            var todo = await context.Todos
+                                        .AsNoTracking()
+                                        .FirstOrDefaultAsync(x => x.Id == id);
+
+            return todo == null ? NotFound() : Ok(todo);
+        }
+
+        [HttpPost]
+        [Route("todos")]
+        public async Task<IActionResult> PostAsync([FromServices]AppDbContext context, 
+                                                   [FromBody]CreateTodoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var todo = new Todo
+            {
+                Date = System.DateTime.Now,
+                Done = false,
+                Type = model.Title
+            };
+
+            try
+            {
+
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();
+                return Created($"v1/todos/{todo.Id}", todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+
+        }
+    }
+}
